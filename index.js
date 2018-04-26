77a5c82435eefee063e669a68b4559a99da4e5c8
Carlo Johnson <cljohnson314@gmail.com>
2018-04-26 21:18:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function whisper (string){
 function logshout (string){
   console.log(string).toUpperCase()
 }
-function logwhisper (string){
+function logWhisper (string){
   console.log(string).toLowerCase()
 }
 function sayHiToGrandma (string){
