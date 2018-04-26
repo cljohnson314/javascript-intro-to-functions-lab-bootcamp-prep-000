70003437524fbf0bdadf478a6e7e5cffb3efd2c2
Carlo Johnson <cljohnson314@gmail.com>
2018-04-26 20:52:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout (string){
+  return string.toUpperCase()
+}
+function whisper (string){
+  return string.toLowerCase()
+}
+function logshout (string){
+  console.log(string).toUpperCase()
+}
+function logwhisper (string){
+  console.log(string).toLowerCase()
+}
+function sayHiToGrandma (string){
+  string.toLowerCase === string  
+  return ("I can\'t hear you")
+  string.toUpperCase === string 
+  return ("YES, INDEED!")
+  string === ("i love you").toUpperCase
+  return ("I love you too")
+}
