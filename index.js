87f470ded1095a620c3505aa1aab8e55fccf2bbb
Carlo Johnson <cljohnson314@gmail.com>
2018-04-26 21:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function shout (string){
 function whisper (string){
   return string.toLowerCase()
 }
-function logshout (string){
+function logShout (string){
   console.log(string).toUpperCase()
 }
 function logWhisper (string){
   console.log(string).toLowerCase()
 }
 function sayHiToGrandma (string){
-  string.toLowerCase === string  
-  return ("I can\'t hear you")
-  string.toUpperCase === string 
+  if (string.toLowerCase() === string){
+  return ("I can\'t hear you")}
+  string.toUpperCase() === string 
   return ("YES, INDEED!")
-  string === ("i love you").toUpperCase
+  string === ("i love you").toUpperCase()
   return ("I love you too")
 }
